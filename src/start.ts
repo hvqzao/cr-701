e6da70d7e56d6eaa1ca2b5689c47d55ebfe5d80b
hvqzao <hvqzao@gmail.com>
2023-04-08 23:28:54 +0200

    Error

--- src/start.ts
+++ src/start.ts
@@ -1,13 +1,15 @@
 import Express from 'express'
 import Session from 'express-session'
-import ConnectRedis from 'connect-redis'
+// import ConnectRedis from 'connect-redis'
+import RedisStore from 'connect-redis'
 import Redis from 'ioredis'
 
 const app = async () => {
-  const RedisStore = ConnectRedis(Session)
+  // const RedisStore = ConnectRedis(Session)
   const redis = new Redis(process.env.REDIS || '')
   const session = Session({
-    store: new RedisStore({
+    // store: new RedisStore({
+    store: RedisStore({
       client: redis
     }),
     secret: process.env.SECRET || ''
