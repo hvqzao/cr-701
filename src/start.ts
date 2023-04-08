de52c848d3e285669fa2b5c7c294e2823024581c
hvqzao <hvqzao@gmail.com>
2023-04-08 23:10:54 +0200

    Init repo

---
+++ src/start.ts
@@ -0,0 +1,19 @@
+import Express from 'express'
+import Session from 'express-session'
+import ConnectRedis from 'connect-redis'
+import Redis from 'ioredis'
+
+const app = async () => {
+  const RedisStore = ConnectRedis(Session)
+  const redis = new Redis(process.env.REDIS || '')
+  const session = Session({
+    store: new RedisStore({
+      client: redis
+    }),
+    secret: process.env.SECRET || ''
+  })
+  const express = Express()
+  express.use(session)
+}
+
+app()
